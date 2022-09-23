28d63e42ab0be2778b94b6e277a4b616cdc395ed
honeyyadav <mr.honeyyadav@gmail.com>
2022-09-23 10:48:29 +0530

    formatting only

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // sudo apt-get install -y nodejs
 // sudo apt-get install chromium-browser
 
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 const NO_OF_USERS = process.argv?.[2] ? parseInt(process.argv[2]) : 5;
 const NO_OF_MINS = process.argv?.[3] ? parseInt(process.argv[3]) : 5;
@@ -13,47 +13,54 @@ const sleep = (sec = 0) => {
     setTimeout(() => {
       res();
     }, sec * 1000);
-  })
-}
+  });
+};
 
 (async () => {
-    const browser = await puppeteer.launch({
-        args: [ '--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream', '--no-sandbox' ],
-        executablePath: '/usr/bin/chromium-browser',
-    })
-    process.on('SIGTERM', async () => {
-      console.info('Closing browser.');
-      await browser.close();
-      process.exit(0);
-    });
-    try {
-      for (let i = 0; i < NO_OF_USERS; i++) {
-        const page = await browser.newPage()
-        console.info(`Adding user ${i}`);
-        await page.goto(`https://rtc-stage.cometchat.io/?sessionID=v1.eu.2181929eb8d4d5fd.tt&username=user${i}&mode=SPOTLIGHT`, {timeout: 0});
-        await sleep(3);
-      }
-      await sleep(10);
-      for (let i = 0; i < NO_OF_MINS * 6; i++) {
-        const userIdAudio = Math.floor(Math.random() * NO_OF_USERS);
-        await browser.pages[userIdAudio].click('#audioButton');
-        console.info(`Toggled audio for user ${userIdAudio}`);
-        await sleep(5);
-        const userIdVideo = Math.floor(Math.random() * NO_OF_USERS);
-        await browser.pages[userIdVideo].click('#videoButton');
-        console.info(`Toggled video for user ${userIdVideo}`);
-        await sleep(5);
-      }
-      for (let i = 0; i < browser.pages.length; i++) {
-        const page = browser.pages[i];
-        await page.close();
-        await sleep(1);
-      }
-      console.info('Closing browser.');
-      await browser.close();
-      process.exit(0);
-    } catch (error) {
-      await browser.close();
-      process.exit(0);
+  const browser = await puppeteer.launch({
+    args: [
+      "--use-fake-ui-for-media-stream",
+      "--use-fake-device-for-media-stream",
+      "--no-sandbox",
+    ],
+    executablePath: "/usr/bin/chromium-browser",
+  });
+  process.on("SIGTERM", async () => {
+    console.info("Closing browser.");
+    await browser.close();
+    process.exit(0);
+  });
+  try {
+    for (let i = 0; i < NO_OF_USERS; i++) {
+      const page = await browser.newPage();
+      console.info(`Adding user ${i}`);
+      await page.goto(
+        `https://rtc-stage.cometchat.io/?sessionID=v1.eu.2181929eb8d4d5fd.tt&username=user${i}&mode=SPOTLIGHT`,
+        { timeout: 0 }
+      );
+      await sleep(3);
     }
-})()
+    await sleep(10);
+    for (let i = 0; i < NO_OF_MINS * 6; i++) {
+      const userIdAudio = Math.floor(Math.random() * NO_OF_USERS);
+      await browser.pages[userIdAudio].click("#audioButton");
+      console.info(`Toggled audio for user ${userIdAudio}`);
+      await sleep(5);
+      const userIdVideo = Math.floor(Math.random() * NO_OF_USERS);
+      await browser.pages[userIdVideo].click("#videoButton");
+      console.info(`Toggled video for user ${userIdVideo}`);
+      await sleep(5);
+    }
+    for (let i = 0; i < browser.pages.length; i++) {
+      const page = browser.pages[i];
+      await page.close();
+      await sleep(1);
+    }
+    console.info("Closing browser.");
+    await browser.close();
+    process.exit(0);
+  } catch (error) {
+    await browser.close();
+    process.exit(0);
+  }
+})();
